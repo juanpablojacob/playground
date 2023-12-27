115d9bd0a4681a26afdb5dba0af719da67ea3fd9
Juan Pablo Jacob <juanpablojacob93@gmail.com>
2023-12-27 16:16:18 -0300

    add auth endpoint

---
+++ api/auth.js
@@ -0,0 +1,21 @@
+import db from "../lib/db.js"
+import { hashPassword } from "../lib/utils.js"
+
+async function authenticateUser(req, res) {
+  const credentials = req.body
+  credentials.password = await hashPassword(credentials.password)
+  const user = await db
+    .collection("users")
+    .findOne(credentials, { projection: { password: 0 } })
+  if (!user) {
+    return res.status(401).send("User not found")
+  }
+  return res.json(user)
+}
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    return await authenticateUser(req, res)
+  }
+  return res.status(405).send("Action not allowed")
+}
